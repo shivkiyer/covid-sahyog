f4aaef4d969cc604a201812399fe82ae0d62f553
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2021-05-12 12:19:08 +0200

    Edited the base environment to use the Sqlite db

--- covidapp/settings/base.py
+++ covidapp/settings/base.py
@@ -15,7 +15,7 @@ from .env import *
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'covidapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': DB_NAME,
-        'HOST': DB_HOST,
-        'PORT': DB_PORT,
-        'USER': DB_USER,
-        'PASSWORD': DB_PASSWORD
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': DB_NAME,
+#         'HOST': DB_HOST,
+#         'PORT': DB_PORT,
+#         'USER': DB_USER,
+#         'PASSWORD': DB_PASSWORD
+#     }
+# }
 
 
 # Password validation
